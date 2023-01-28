d7e2e244cb02920bbbf3df86183597b63293b825
Anastasia <anastasia.frontend1912@gmail.com>
2023-01-28 13:08:40 +0300

    completed

--- script.js
+++ script.js
@@ -61,3 +61,24 @@ function initializeClock(id, endtime) {
  
 var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); 
 initializeClock('timer', deadline);
+
+
+
+
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-33.33333333333333%)";
+      e.target.classList.add("active");
+    } 
+  }
+});
